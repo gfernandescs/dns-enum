aa390859c83549403259ac523f0c26dfe69970c4
root <root@localhost.localdomain>
2019-11-03 17:04:52 -0300

    Refact

--- dnsEnum.sh
+++ dnsEnum.sh
@@ -1,45 +1,147 @@
-#script para enumeracao completa de dns
-#!/bin/bash
+#!/usr/bin/env bash
 
-#variaveis armazenando cores
-green=$'\e[0;32m'
-purple=$'\e[0;35m'
-nc=$'\033[0m'
+################################################################################
+# Titulo    : dns-enum                                                         #
+# Versao    : 1.0                                                              #
+# Data      : 03/11/2019                                                       #
+# Tested on : Linux                                                            #
+################################################################################
 
-if [ "$1" == "" ]
-then
-echo "#######################################################################"
-echo "                                                                       "
-echo "-----------------------------------------------------------------------"
-echo "--------------------- xFoo0T - DnsEnum - v1.0 -------------------------"
-echo "-----------------------------------------------------------------------"
-echo "                                                                       "
-echo "     ----- Uso Padrão: ./dsnEnum.sh alvo.com.br worldlist.txt -----    "
-echo "    @@  worldlist para brute-force para descoberta de subdominios @@   "
-echo "                                                                       "
-echo "#######################################################################"
-exit 0
-fi
+# ==============================================================================
+# Constantes
+# ==============================================================================
 
-#Descobrindo name servers (NS)
-echo "${purple}############################################"
-echo "           ${green} Name Servers${nc}        "
-echo "${purple}############################################${nc}"
-host -t ns $1 | cut -d " " -f 4
+# Constantes para facilitar o uso de cores.
+GREEN=$'\e[0;32m'
+PURPLE=$'\e[0;35m'
+END=$'\033[0m'
+RED='\033[31;1m'
 
-#Descobrindo mail servers(MX)
-echo "${purple}###########################################"
-echo "           ${green} Mail Servers${nc}                   "
-echo "${purple}###########################################${nc}"
-host -t mx $1 | cut -d " " -f 7
+# Constantes armazenando os valores dos argumentos
+ARG_01=$1
+ARG_02=$2
 
-#Tentando tranferir zona de todos os name servers
-echo "${purple}##########################################"
-echo "         ${green}Tentando transferir zona${nc}         "
-echo "${purple}##########################################${nc}"
-for server in $(host -t ns $1 | cut -d " " -f4)
-do
-host -l $1 $server
-echo " "
-done
+# Constante armazenando a versão do programa.
+VERSION='1.0'
+
+
+# ==============================================================================
+# Banner do programa
+# ==============================================================================
+
+__Banner__() {
+    echo "#######################################################################"
+    echo "                                                                       "
+    echo "-----------------------------------------------------------------------"
+    echo "--------------------- xFoo0T - DnsEnum - v1.0 -------------------------"
+    echo "-----------------------------------------------------------------------"
+    echo "                                                                       "
+    echo "     ----- Uso Padrão: ./dsnEnum.sh alvo.com.br worldlist.txt -----    "
+    echo "    @@  worldlist para brute-force para descoberta de subdominios @@   "
+    echo "                                                                       "
+    echo "#######################################################################"
+    exit 0
+}
+
+# ==============================================================================
+# Menu de ajuda
+# ==============================================================================
+
+__Help__() {
+    printf "\
+    \nNAME\n \
+    \t$0 - Software para realizar a enumeração completa de DNS do host informado.\n \
+    \nSYNOPSIS\n \
+    \t$0 [Options] [URL]\n \
+    \nOPTIONS\n \
+    \t-h, --help\n \
+    \t\tMostra o menu de ajuda.\n\n \
+    \t-v, --version\n \
+    \t\tMostra a versão do programa.\n\n \
+    \n"
+}
+
+# ==============================================================================
+# Verificação básica
+# ==============================================================================
+
+__basicCheck__() {
+    # Verificando as dependências.
+    if ! [[ -e /usr/bin/host ]]; then
+        printf "\nFaltando programa ${RED}host${END} para funcionar.\n"
+        exit 1
+    fi
+
+    # Verificando se não foi passado argumentos.
+    if [[ $ARG_01 == "" ]]; then
+        __Banner__
+        exit 1
+    fi
+}
+
+# ==============================================================================
+# Descobrindo name servers (NS)
+# ==============================================================================
+
+__discoverNameServers__() {
+    echo "${PURPLE}############################################"
+    echo "           ${GREEN} Name Servers${END}        "
+    echo "${PURPLE}############################################${END}"
+    host -t ns $ARG_01 | cut -d " " -f 4
+}
+
+# ==============================================================================
+# Descobrindo mail servers(MX)
+# ==============================================================================
+
+__discoverMailServers__() {
+    echo "${PURPLE}###########################################"
+    echo "           ${GREEN} Mail Servers${END}                   "
+    echo "${PURPLE}###########################################${END}"
+    host -t mx $ARG_01 | cut -d " " -f 7
+}
+
+# ==============================================================================
+# Tentando transferir zona de todos os name servers
+# ==============================================================================
+
+__transferZone__() {
+    echo "${PURPLE}##########################################"
+    echo "         ${GREEN}Tentando transferir zona${END}         "
+    echo "${PURPLE}##########################################${END}"
+    for server in $(host -t ns $ARG_01 | cut -d " " -f4)
+    do
+        host -l $ARG_01 $server
+        echo " "
+    done
+} 
+
+# ==============================================================================
+# Função principal do programa
+# ==============================================================================
+
+__Main__() {
+    __basicCheck__
+
+   case $ARG_01 in
+        "-v"|"--version") printf "\nVersion: $VERSION\n"
+              exit 0
+        ;; 
+        "-h"|"--help") __Help__
+              exit 0
+        ;;
+        *) __discoverNameServers__
+           __discoverMailServers__
+           __transferZone__
+        ;; 
+	esac  
+}
+
+# ==============================================================================
+# Inicio do programa
+# ==============================================================================
+
+__Main__
+
+
 
